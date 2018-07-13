e256f730c86a4dd0b21ba63a0ca005b0082fbd74
paulserafini <pserafin@sfu.ca>
2018-07-13 16:37:45 -0700

    Add help text to sidebar of CSV generator

--- CSV Generator/server.R
+++ CSV Generator/server.R
@@ -20,10 +20,11 @@ shinyServer(function(input, output, session) {
                         callback = htmlwidgets::JS(
                         "function (key, options) {
                             var csv = csvString(this, sep=',', dec='.');
+
+                            // Remove the row and column names
                             while (csv[0] == 'X') {
                             csv = csv.substring(csv.indexOf('X')+14);
                             }
-
 
                             var link = document.createElement('a');
                             link.setAttribute('href', 'data:text/plain;charset=utf-8,' +
@@ -39,16 +40,16 @@ shinyServer(function(input, output, session) {
   
   output$corrtable2 <- renderRHandsontable({
     
-    variables <- as.numeric(input$variables)
+      variables <- as.numeric(input$variables)
     
-    MAT <- matrix('', nrow=variables, ncol=variables)
+      MAT <- matrix('', nrow=variables, ncol=variables)
     
-    rownames(MAT) <- paste("X<sub>", 1:variables, "</sub>", sep="")
-    colnames(MAT) <- paste("X<sub>", 1:variables, "</sub>", sep="")
+      rownames(MAT) <- paste("X<sub>", 1:variables, "</sub>", sep="")
+      colnames(MAT) <- paste("X<sub>", 1:variables, "</sub>", sep="")
     
-    rhandsontable(MAT) %>%
-      hot_context_menu(allowRowEdit = FALSE, allowColEdit = FALSE) %>%
-      hot_cols(renderer = "
+      rhandsontable(MAT) %>%
+          hot_context_menu(allowRowEdit = FALSE, allowColEdit = FALSE) %>%
+          hot_cols(renderer = "
                function (instance, td, row, col, prop, value, cellProperties) {
                Handsontable.renderers.NumericRenderer.apply(this, arguments);
                if (row == col) {
@@ -74,83 +75,80 @@ shinyServer(function(input, output, session) {
                }",format='0.[000]')
     })
   
-  
-  
-  
-  
-  output$hypothesistable <- renderRHandsontable({
+
+    output$hypothesistable <- renderRHandsontable({
     
-    # Ensure that the correlation matrix exists; if so assign it a variable name
-    validate(need(input$corrtable2, ""))
-    h1 <- hot_to_r(input$corrtable2)
+        ## Ensure that the correlation matrix exists; if so assign it a variable name
+        validate(need(input$corrtable2, ""))
+        h1 <- hot_to_r(input$corrtable2)
     
-    ## Initialize hypothesis matrix with an empty row + column names
-    h_matrix <- matrix(c('','','','',''),nrow=1,ncol=5)
-    colnames(h_matrix) <- c('group','row','column','parameter tag','fixed value')
+        ## Initialize hypothesis matrix with an empty row + column names
+        h_matrix <- matrix(c('','','','',''),nrow=1,ncol=5)
+        colnames(h_matrix) <- c('group','row','column','parameter tag','fixed value')
     
-    ## Loop through every value in the correlation matrix
-    for (r in 1:nrow(h1)) {
-      for (c in 1:ncol(h1)) {
-        
-        ## For values below the diagonal, i.e. group 1,...
-        if (r > c) {
-          val <- h1[r,c] 
-          if (!(is.na(as.numeric(val)))) { # If they are numeric...
-            val <- as.numeric(val)
-            if(abs(val) < 1){ # If they are a decimal number, make them a fixed value
-              new_row <- c(1, r, c, 0 ,h1[r,c])
-              h_matrix <- rbind(h_matrix, new_row)
-            }else if(!(grepl("\\D", val))){ # If they are a positive integer, make them a parameter tag
-              new_row <- c(1, r, c, val ,0)
-              h_matrix <- rbind(h_matrix, new_row)
+        ## Loop through every value in the correlation matrix
+        for (r in 1:nrow(h1)) {
+            for (c in 1:ncol(h1)) {
+                
+                ## For values below the diagonal, i.e. group 1,...
+                if (r > c) {
+                    val <- h1[r,c] 
+                    if (!(is.na(as.numeric(val)))) { # If they are numeric...
+                        val <- as.numeric(val)
+                        if(abs(val) < 1){ # If they are a decimal number, make them a fixed value
+                            new_row <- c(1, r, c, 0 ,h1[r,c])
+                            h_matrix <- rbind(h_matrix, new_row)
+                        }else if(!(grepl("\\D", val))){ # If they are a positive integer, make them a parameter tag
+                            new_row <- c(1, r, c, val ,0)
+                            h_matrix <- rbind(h_matrix, new_row)
+                        }
+                    }
+                }
+                
+                ## For values above the diagonal, i.e. group 2...
+                if (r < c) {
+                    val <- h1[r,c] 
+                    if (!(is.na(as.numeric(val)))) {
+                        val <- as.numeric(val)
+                        if(abs(val) < 1){
+                            new_row <- c(2, r, c, 0 ,h1[r,c])
+                            h_matrix <- rbind(h_matrix, new_row)
+                        }else if(!(grepl("\\D", val))){
+                            new_row <- c(2, c, r, val ,0)
+                            h_matrix <- rbind(h_matrix, new_row)
+                        }
+                    }
+                }
             }
-          }
         }
-        
-        ## For values above the diagonal, i.e. group 2...
-        if (r < c) {
-          val <- h1[r,c] 
-          if (!(is.na(as.numeric(val)))) {
-            val <- as.numeric(val)
-            if(abs(val) < 1){
-              new_row <- c(2, r, c, 0 ,h1[r,c])
-              h_matrix <- rbind(h_matrix, new_row)
-            }else if(!(grepl("\\D", val))){
-              new_row <- c(2, c, r, val ,0)
-              h_matrix <- rbind(h_matrix, new_row)
-            }
-          }
+    
+        ## Delete the initial, empty row of the hypothesis matrix if there is more than one row
+        if (h_matrix[1,] == c('','','','','') && nrow(h_matrix) > 1) {
+            h_matrix <- h_matrix[-1, , drop=FALSE]
         }
-      }
-    }
     
-    ## Delete the initial, empty row of the hypothesis matrix if there is more than one row
-    if (h_matrix[1,] == c('','','','','') && nrow(h_matrix) > 1) {
-      h_matrix <- h_matrix[-1, , drop=FALSE]
-    }
-    
-    ## Delete row names
-    rownames(h_matrix) <- NULL
+        ## Delete row names
+        rownames(h_matrix) <- NULL
 
-    ## Make the hypothesis matrix downloadable via context menu
-    rhandsontable(h_matrix) %>%
-        hot_context_menu(
-            customOpts = list(
-                csv = list(name = "Download to CSV",
-                        callback = htmlwidgets::JS(
-                        "function (key, options) {
-                            var csv = csvString(this, sep=',', dec='.');
-                            var csv = csv.substring(43);
+        ## Make the hypothesis matrix downloadable via context menu
+        rhandsontable(h_matrix) %>%
+            hot_context_menu(
+                customOpts = list(
+                    csv = list(name = "Download to CSV",
+                            callback = htmlwidgets::JS(
+                            "function (key, options) {
+                                var csv = csvString(this, sep=',', dec='.');
+                                var csv = csv.substring(43); // Remove the header
 
-                            var link = document.createElement('a');
-                            link.setAttribute('href', 'data:text/plain;charset=utf-8,' +
-                            encodeURIComponent(csv));
-                            link.setAttribute('download', 'hypothesis.csv');
+                                var link = document.createElement('a');
+                                link.setAttribute('href', 'data:text/plain;charset=utf-8,' +
+                                encodeURIComponent(csv));
+                                link.setAttribute('download', 'hypothesis.csv');
 
-                            document.body.appendChild(link);
-                            link.click();
-                            document.body.removeChild(link);
-                        }"))))
+                                document.body.appendChild(link);
+                                link.click();
+                                document.body.removeChild(link);
+                            }"))))
 
   })
   
--- CSV Generator/ui.R
+++ CSV Generator/ui.R
@@ -4,23 +4,23 @@ require(rhandsontable) || install.packages(rhandsontable)
 
 shinyUI(fluidPage(theme = "simplex.css",
 
-                  HTML('<br>
-                        <link rel="stylesheet" type="text/css" href="index.css">
-                        <style>
-                          html { overflow-y: scroll; }
-                        </style>
-                        <div class="bar">
-                          <b class="title">Measurement and Modelling Lab &nbsp; - &nbsp; Tools</b><br class="rwd-break">
-                          <b class="link">
-                            <a href="https://shiny.rcg.sfu.ca/u/pserafin/rsquared/"><font color="white">MML-R2</font></a>
-                            &emsp;&nbsp;<a href="https://shiny.rcg.sfu.ca/u/pserafin/wbcorr/"><font color="white">MML-WBCORR</font></a>
-                            &emsp;&nbsp;<a href="https://shiny.rcg.sfu.ca/u/pserafin/csvgenerator/"><font color="#00ca8a">CSV Generator</font></a>
-                          </b>
-                        </div>
-                        <br>'
-                       ),
+    HTML('<br>
+            <link rel="stylesheet" type="text/css" href="index.css">
+            <style>
+            html { overflow-y: scroll; }
+            </style>
+            <div class="bar">
+            <b class="title">Measurement and Modelling Lab &nbsp; - &nbsp; Tools</b><br class="rwd-break">
+            <b class="link">
+                <a href="https://shiny.rcg.sfu.ca/u/pserafin/rsquared/"><font color="white">MML-R2</font></a>
+                &emsp;&nbsp;<a href="https://shiny.rcg.sfu.ca/u/pserafin/wbcorr/"><font color="white">MML-WBCORR</font></a>
+                &emsp;&nbsp;<a href="https://shiny.rcg.sfu.ca/u/pserafin/csvgenerator/"><font color="#00ca8a">CSV Generator</font></a>
+            </b>
+            </div>
+            <br>'
+        ),
 
-  tags$head(
+    tags$head(
            tags$style(HTML("@import url('//fonts.googleapis.com/css?family=Patua+One');
                           h1 {
                           font-family: 'Patua One';
@@ -33,31 +33,30 @@ shinyUI(fluidPage(theme = "simplex.css",
                            ))
        ),
 
-  headerPanel('', windowTitle = 'CSV Generator'),
+    headerPanel('', windowTitle = 'CSV Generator'),
   
-  sidebarLayout(
-      sidebarPanel(
-          selectInput("file",
-                      label = "File to create:", 
-                      choices = list("Correlation matrix" = "correlation", "Hypothesis matrix" = "hypothesis"), 
-                      selected = "correlation"
-          ),
-          sliderInput("variables",
-                      "Number of variables:",
-                      min = 2,
-                      max = 16,
-                      value = 2
-          ),
-          conditionalPanel(condition = "input.tabs == 'Correlation Matrices'",
-                           helpText("Note: Right click on a correlation matrix to download it as a .csv file. Unfortunately this feature does not work in all browsers,
-                                     but it is confirmed to work in Firefox and Chrome.")
-          ),
-          conditionalPanel(condition = "input.tabs == 'Hypothesis Matrix'", helpText("Note: Cells containing the same positive integer are hypothesised to be equal,
-                                                                                      and cells containing a value between -1 and 1 are hypothesised to be equal to that value.
-                                                                                      Right click on the hypothesis matrix to download it as a .csv file.
-                                                                                      Unfortunately this feature does not work in all browsers, but it is confirmed to work in Firefox and Chrome.")
-          )
-      ),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("file",
+                        label = "File to create:", 
+                        choices = list("Correlation matrix" = "correlation", "Hypothesis matrix" = "hypothesis"), 
+                        selected = "correlation"
+            ),
+            sliderInput("variables",
+                        "Number of variables:",
+                        min = 2,
+                        max = 16,
+                        value = 2
+            ),
+            conditionalPanel(condition = "input.file == 'correlation'",
+                             helpText("Note: Right click on a correlation matrix to download it as a .csv file.")
+            ),
+            conditionalPanel(condition = "input.file == 'hypothesis'",
+                             helpText("Note: Cells containing the same positive integer are hypothesised to be equal,
+                                       and cells containing a value between -1 and 1 are hypothesised to be equal to that value.
+                                       Right click on the hypothesis matrix to download it as a .csv file.")
+            )
+        ),
 
       mainPanel(
         conditionalPanel(condition = "input.file == 'correlation'", 
