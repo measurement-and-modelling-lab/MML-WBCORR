8d38fd2eb1de073c895eebb9d17478f3fb88c332
paulserafini <pserafin@sfu.ca>
2018-07-13 18:21:16 -0700

    Add a ton of comments to CSV generator files

--- CSV Generator/ui.R
+++ CSV Generator/ui.R
@@ -1,9 +1,11 @@
+## Load the required libraries and install them if necessary
 require(shiny) || install.packages(shiny)
 require(shinythemes) || install.packages(shinythemes)
 require(rhandsontable) || install.packages(rhandsontable)
 
 shinyUI(fluidPage(theme = "simplex.css",
 
+    ## Header bar with links to other apps
     HTML('<br>
             <link rel="stylesheet" type="text/css" href="index.css">
             <style>
@@ -20,6 +22,7 @@ shinyUI(fluidPage(theme = "simplex.css",
             <br>'
         ),
 
+    ## CSS style stuff
     tags$head(
            tags$style(HTML("@import url('//fonts.googleapis.com/css?family=Patua+One');
                           h1 {
@@ -33,6 +36,7 @@ shinyUI(fluidPage(theme = "simplex.css",
                            ))
        ),
 
+    ## No title since it's in the header bar
     headerPanel('', windowTitle = 'CSV Generator'),
   
     sidebarLayout(
@@ -48,9 +52,13 @@ shinyUI(fluidPage(theme = "simplex.css",
                         max = 16,
                         value = 2
             ),
+
+            ## Only created if the file being created is a correlation matrix
             conditionalPanel(condition = "input.file == 'correlation'",
                              helpText("Note: Right click on a correlation matrix to download it as a .csv file.")
             ),
+
+            ## Only created if the file being created is a hypothesis matrix
             conditionalPanel(condition = "input.file == 'hypothesis'",
                              helpText("Note: Cells containing the same positive integer are hypothesised to be equal,
                                        and cells containing a value between -1 and 1 are hypothesised to be equal to that value.
@@ -60,15 +68,23 @@ shinyUI(fluidPage(theme = "simplex.css",
         ),
 
       mainPanel(
+
+        ## Javascript table for creating a correlation matrix
         conditionalPanel(condition = "input.file == 'correlation'", 
                          rHandsontableOutput("corrtable1")
         ),
+
+        ## Javascript table for specifying hypotheses
         conditionalPanel(condition = "input.file == 'hypothesis'", 
           rHandsontableOutput("corrtable2")
         ),
+
+        ## Space between the two tables for creating a hypothesis matrix
         conditionalPanel(condition = "input.file == 'hypothesis'", 
           HTML("<br>")
         ),
+
+        ## Javascript table for generating and downloading the hypothesis matrix
         conditionalPanel(condition = "input.file == 'hypothesis'", 
                          rHandsontableOutput("hypothesistable")
         )
@@ -76,7 +92,9 @@ shinyUI(fluidPage(theme = "simplex.css",
 
   ),
   
-  HTML('<br><br><br><br><br><br><br><br><br><br>'),
+  HTML('<br><br><br><br><br><br><br><br><br><br>'), ## Breathing room
+
+  ## Footer bar
   HTML('<link rel="stylesheet" type="text/css" href="index.css">
         <div class="bar2">
           <b class="bottom">
