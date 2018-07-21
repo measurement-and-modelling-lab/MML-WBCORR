028bd4042aa5838656d873dc9c5b1be56191a795
paulserafini <pserafin@sfu.ca>
2018-07-20 20:05:04 -0700

    Add .gitattributes

--- server.R
+++ server.R
@@ -192,7 +192,6 @@ shinyServer(function(input, output, session) {
                 html.output <- paste0(html.output, htmlTable(MardiaSK[[1]], align="c", caption="Assessment of the Distribution of the Observed Marginals"))
                 html.output <- paste0(html.output, htmlTable(MardiaSK[[2]], align="c", caption="Assessment of Multivariate Normality"))
             } else {
-                cat("\nAssessment of Multivariate Normality\n")
                 html.output <- paste0(html.output, htmlTable(MardiaSK[[1]], align="c", caption="Assessment of Multivariate Skewness"))
                 html.output <- paste0(html.output, htmlTable(MardiaSK[[2]], align="c", caption="Assessment of Multivariate Kurtosis"))
             }
